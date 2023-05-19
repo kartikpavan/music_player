fa016cf9353a763aea25cbeca38d7625b66d015c
kartikpavan <kartik.varanasi@yahoo.com>
2023-05-19 23:54:01 +0530

    code refactor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import { Home, Login, NotFoundPage } from "./components";
 
 const App = () => {
-   return (
-      <div className="w-screen h-screen  flex justify-center items-center">
-         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<NotFoundPage />} />
-         </Routes>
-      </div>
-   );
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
