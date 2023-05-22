84f930a924771cb507875d861118cacf2f7584e9
kartikpavan <kartik.varanasi@yahoo.com>
2023-05-22 15:10:16 +0530

    (client):(LOGIN):navigate back to homescreen when logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,43 @@
-import { onAuthStateChanged } from "firebase/auth";
-import React, { useEffect, useState } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { firebaseAuth } from "../config/firebase.config";
-import { Home, Login, Notfound } from "./pages";
+import React, {useEffect, useState} from "react";
+import {Route, Routes, useNavigate} from "react-router-dom";
+import {firebaseAuth} from "../config/firebase.config";
+import {Home, Login, Notfound} from "./pages";
 
 const App = () => {
-  const navigate = useNavigate();
-  //if auth is false,navigate user to LOGIN screen
-  const [auth, setAuth] = useState(false || window.localStorage.getItem("auth") === "true");
+   const navigate = useNavigate();
+   //if auth is false,navigate user to LOGIN screen
+   const [authStatus, setAuthStatus] = useState(false || window.localStorage.getItem("auth") === "true");
 
-  // Firebase Auth Observer
-  useEffect(() => {
-    onAuthStateChanged(firebaseAuth, (user) => {
-      /*
-      -If user is logged in , access the refresh Token and pass it to Backend for verification
-      -Redirect User from Login page to HOME screen
-      */
-      if (user) {
-        user.getIdToken().then((token) => {
-          console.log(token);
-        });
-        console.log(user);
-      } else {
-        window.localStorage.setItem("auth", "false");
-        navigate("/login");
+   // Firebase Auth Observer
+   useEffect(() => {
+      firebaseAuth.onAuthStateChanged((userCred) => {
+         if (userCred) {
+            // if user exist then send the token id to backend for verification
+            userCred.getIdToken().then(token => console.log(token))
+         } else {
+            setAuthStatus(false)
+            window.localStorage.setItem("auth", "false");
+            navigate('/login')
+         }
+      })
+   }, []);
+
+   // checking if local storage auth state is true
+   useEffect(() => {
+      if (window.localStorage.getItem("auth") === "true") {
+         navigate("/", {replace: true});
       }
-    });
-  }, []);
+   }, []);
 
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login setAuth={setAuth} />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </div>
-  );
+   return (
+       <div className="w-screen h-screen flex justify-center items-center">
+          <Routes>
+             <Route path="/" element={<Home/>}/>
+             <Route path="/login" element={<Login setAuthStatus={setAuthStatus}/>}/>
+             <Route path="*" element={<Notfound/>}/>
+          </Routes>
+       </div>
+   );
 };
 
 export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,42 +1,52 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import bgVideo from "../assets/bgVideo2.mp4";
-
 // Firebase imports
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import { firebaseAuth } from "../../config/firebase.config";
 
-const Login = () => {
-  // Google login
-  const provider = new GoogleAuthProvider();
-  const loginWithGoogle = async () => {
-    const result = await signInWithRedirect(firebaseAuth, provider);
-    if (result) {
-      setAuth(true);
-      window.localStorage.setItem("auth", "true");
-    }
-  };
+const Login = ({ setAuthStatus }) => {
+   const navigate = useNavigate();
+   const googleProvider = new GoogleAuthProvider();
+   const loginWithGoogle = async () => {
+      await signInWithPopup(firebaseAuth, googleProvider).then((userCred)=>{
+         if(userCred){
+            setAuthStatus(true);
+            window.localStorage.setItem("auth","true");
+            // if the user is logged in, redirect to home page
+            firebaseAuth.onAuthStateChanged((user)=>{
+               if(user){
+                  navigate("/",{replace:true})
+               }else{
+                  setAuthStatus(false);
+                  navigate('/login')
+               }
+            })
+         }
+      })
+   };
 
-  return (
-    <main className="relative w-screen h-screen">
-      {/* background Video */}
-      <video
-        src={bgVideo}
-        autoPlay
-        loop
-        muted
-        className="h-full w-full object-none lg:object-fill"
-      />
-      <div className="absolute inset-0 flex items-center justify-center p-4 ">
-        <div onClick={loginWithGoogle}>
-          <div className="shadow-lg rounded-md px-8 py-4 text-white hover:text-black bg-lightOverlay flex items-center gap-3 backdrop-blur-md hover:bg-card duration-200 transition-all cursor-pointer">
-            <FcGoogle size={30} />
-            <p>Sign in with Google</p>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+   return (
+      <main className="relative w-screen h-screen">
+         {/* background Video */}
+         <video
+            src={bgVideo}
+            autoPlay
+            loop
+            muted
+            className="h-full w-full object-none lg:object-fill"
+         />
+         <div className="absolute inset-0 flex items-center justify-center p-4 ">
+            <div >
+               <div onClick={loginWithGoogle} className="shadow-lg rounded-md px-8 py-4 text-white hover:text-black bg-lightOverlay flex items-center gap-3 backdrop-blur-md hover:bg-card duration-200 transition-all cursor-pointer">
+                  <FcGoogle size={30} />
+                  <p>Sign in with Google</p>
+               </div>
+            </div>
+         </div>
+      </main>
+   );
 };
 
 export default Login;
